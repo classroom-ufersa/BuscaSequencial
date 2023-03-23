1b9829809fb0fe7421a61d2c1a952331f323a6e5
Gabriellk28 <uyanelol2016@gmail.com>
2023-03-23 07:51:46 -0300

    principal

--- aluno.c
+++ aluno.c
@@ -1,45 +1,9 @@
 #include<stdio.h>
 #include<string.h>
-#include"aluno.h"
-
-struct aluno
-{
-    char nome[50];
-    int matricula;
-    int documento;
-};
-
-int registraAluno(char *arquivo)
-{
-    //Declaração da variaveis e ponteiro.
-    Aluno aluno;
-    FILE *fp;
-    //w(whrite) para criar o arquivo caso não exista.
-    fp=fopen(arquivo, "a"); 
-    if(fp==NULL){
-        printf("Erro ao abrir o arquivo\n");
-        return 1;
-    }
-
-    //Imprime na tela do usuário.
-    printf("Informe o nome:\n");
-    scanf(" %[^\n]s", aluno.nome);
-    printf("Informe o número da matricula:\n");
-    scanf(" %d", &aluno.matricula);
-    printf("Informe o número do documento:\n");
-    scanf(" %d", &aluno.documento);
-    
-    //Escreve os dados que o usuário digitou no arquivo.
-    fprintf(fp, "%s %d %d\n", aluno.nome, aluno.matricula, aluno.documento);
-
-    //fecha o arquivo
-    fclose(fp);
-}
-#include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
 #include"aluno.h"
 
+//Struct para os dados do aluno.
 struct aluno
 {
     char nome[50];
@@ -53,13 +17,16 @@ int registraAluno(char *arquivo)
     //Declaração da variaveis e ponteiro.
     Aluno aluno;
     FILE *fp;
+
+    //Testa se o arquivo foi aberto.
+    //Abertura com "a" para salvar dados já existentes no arquivo.
     fp=fopen(arquivo, "a"); 
     if(fp==NULL){
         printf("Erro ao abrir o arquivo\n");
         return 1;
     }
 
-    //Imprime na tela do usuário.
+    //Imprime as seguintes menssagens na tela do usuário.
     printf("Informe o nome:\n");
     scanf(" %[^\n]s", aluno.nome);
     printf("Informe o número da matricula:\n");
